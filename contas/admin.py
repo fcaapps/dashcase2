3de7ff988b2456c19f501ea79ef7fb28692d7122
fcaapps <fca.apps@gmail.com>
2018-10-26 19:03:01 -0300

    Correção de Usuário - Criação de Custom E-mail

--- contas/admin.py
+++ contas/admin.py
@@ -4,7 +4,19 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email')
+    list_display = ('custom_email', 'username')
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('custom_email', 'username', 'password1', 'password2')}
+         ),
+    )
+    fieldsets = (
+        (None, {'fields': ('custom_email', 'password')}),
+        ('Informações Pessoais', {'fields': ('username',)}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('Datas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
 
 admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
--- contas/models.py
+++ contas/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 class User(AbstractUser):
 
-    email = models.EmailField(
-        verbose_name='E-mail',
+    custom_email = models.EmailField(
+        verbose_name='Usuário / E-mail',
         max_length=255,
         unique=True,
     )
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    username = models.CharField(
+        error_messages={'unique': 'A user with that username already exists.'},
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150,
+        validators=[UnicodeUsernameValidator()], verbose_name='Apelido'
+    )
+
+    USERNAME_FIELD = 'custom_email'
+    REQUIRED_FIELDS = ['username', 'email']
 
     class Meta:
         verbose_name = 'Usuário'
