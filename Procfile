b14a831d3435090fa6f29a5e1c81c5f609bbff43
fcaapps <fca.apps@gmail.com>
2018-10-26 14:26:32 -0300

    Deploy Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dashcase.wsgi --log-file -
--- dashcase/settings.py
+++ dashcase/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%4%6=f9k_)*kt5kd^yc=r@i892c@3hetxr^9ju%ebf*#fd48pa'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['dashcase.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -78,12 +79,16 @@ WSGI_APPLICATION = 'dashcase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
@@ -137,3 +142,6 @@ LOGIN_REDIRECT_URL = 'home'
 
 # USUÁRIO CUSTOMIZADO
 AUTH_USER_MODEL = 'contas.User'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
